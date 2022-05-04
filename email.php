3fec972e919463cbe0d26a04f0b0892804e0e43f
mestrypranali06 <mestrypranali06@gmmail.com>
2022-05-04 23:17:52 +0530

    email form. #3

---
+++ email.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Cars | Santro</title>
+  </head>
+  <body>
+	<h1 style="color: red;">SANTRO BODY</h1>
+	
+	<form action="mailto:hermione@potionfy.com" method="post" enctype="text/plain">
+Name:<br>
+    <input type="text" name="name"><br>
+    E-mail:<br>
+    <input type="text" name="mail"><br>
+    <input type="submit" value="Send">
+    <input type="reset" value="Reset">
+</form>
+
+</body>
+</html>

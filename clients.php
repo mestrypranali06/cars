212a49d505997cc97ce1c5a3f8167915b9351067
mestrypranali06 <mestrypranali06@gmmail.com>
2022-05-06 17:57:38 +0530

    added all clients list. #8

--- clients.php
+++ clients.php
@@ -5,5 +5,21 @@
   </head>
   <body>
   
+	  <ul>
+	  
+		<li>
+			Peter Inc
+		</li>
+		
+		<li>
+			Trex Corp
+		</li>
+		
+		<li>
+			Al-Habibi Pvt Ltd
+		</li>
+		
+	  </ul>
+  
   </body>
 </html>

08f00689588379d705c3f17306d8edc25b686ed0
mestrypranali06 <84989588+mestrypranali06@users.noreply.github.com>
2022-05-06 17:33:20 +0530

    added design elements to clients page. #8.

--- clients.php
+++ clients.php
@@ -2,8 +2,22 @@
 <html>
   <head>
     <title>Cars | Clients</title>
+    
+    <style>
+      .ulist{
+          background-color: lightgray;
+      }
+    </style>
   </head>
   <body>
+    
+    <div class="container">
+    
+      <ul class"ulist">
+
+      </ul>
+      
+    </div>
   
   </body>
 </html>
